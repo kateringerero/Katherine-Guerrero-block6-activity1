7316c05fcdeb8d8d9ab426d9072d8e36ab3421a3
KITTIE\guerr <kaum.guerrero.up@phinmaed.com>
2023-12-05 13:46:58 +0800

    First commit

---
+++ app/src/main/java/com/guerrero/katherine/block6/p1/quiz/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.guerrero.katherine.block6.p1.quiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.guerrero.katherine.block6.p1.quiz.ui.theme.KatherineGuerreroBlock6QuizTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KatherineGuerreroBlock6QuizTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "\nHello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KatherineGuerreroBlock6QuizTheme {
+        Greeting("Android")
+    }
+}
